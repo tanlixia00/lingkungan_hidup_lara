5e8dbaaf9829fb75ad5673ce4c5a12d9b7b27e7b
tanlixia00 <62798315+tanlixia00@users.noreply.github.com>
2021-07-13 15:05:32 +0700

    disabled textarea

--- resources/views/frontend/detail.blade.php
+++ resources/views/frontend/detail.blade.php
@@ -12,12 +12,14 @@
             @csrf
             <div class="form-body">
                 <div>
-                    <label for="inputfile">Gambar Satwa</label> <br>
-                    <img src="{{ asset('images/'.$data->gambar) }}" alt="Current image" height="300" width="300">
+                    <!-- <label for="inputfile">Gambar Satwa</label> <br> -->
+                    <img src="{{ asset('images/'.$data->gambar) }}" alt="Current image" height="300" width="300" >
+                    <br>
                 </div>
+                <br>
                 <div class="form-group">
                     <label>Nama Satwa</label>
-                    <input type="text" class="form-control" name="namaSatwa" id="namaSatwa" placeholder="" value="{{$data->nama}}">
+                    <input type="text" class="form-control" name="namaSatwa" id="namaSatwa" placeholder="" value="{{$data->nama}}" readonly >
                     <span class="help-block">
                      </span>
                 </div>
@@ -25,15 +27,15 @@
                 
                 <div class="form-group">
                     <label>Spesies</label>
-                    <input type="text" class="form-control" id="spesies" name="spesies" placeholder="total stok"  value="{{$data->spesies}}">
+                    <input type="text" class="form-control" id="spesies" name="spesies" placeholder="total stok"  value="{{$data->spesies}}" readonly >
                 </div>
                 <div class="form-group">
                     <label>Asal</label>
-                    <input type="text" class="form-control" id="asal" name="asal" placeholder="berisi asal"  value="{{$data->asal}}">
+                    <input type="text" class="form-control" id="asal" name="asal" placeholder="berisi asal"  value="{{$data->asal}}" readonly >
                 </div>
                 <div class="form-group">
                     <label>Deskripsi</label> <br>
-                    <textarea name="desc" id="desc" cols="140" rows="5" value="">{{$data->deskripsi}}</textarea>
+                    <textarea disabled name="desc" id="desc" cols="140" rows="5" value="">{{$data->deskripsi}}</textarea>
                 </div>
                 
             </div>
